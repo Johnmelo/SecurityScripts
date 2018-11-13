3b29e99add2a80f1236261dfde705b74419427ea
Jonathas <johnjonathas0@gmail.com>
2018-11-13 00:58:51 -0200

    Update getMails.sh
    remove comments.

--- getMails.sh
+++ getMails.sh
@@ -2,7 +2,7 @@
 if [ "$1" == "" ];
 then
 echo "forma de uso: "
-echo "./webParsing.sh target.com"
+echo "./getMails.sh target.com"
 else
 echo "####################################################"
 echo "BUSCANDO EMAILS"
@@ -11,14 +11,4 @@ echo "####################################################"
 wget -nv --output-document index.html "$1" 
 
 cat index.html | grep "@" > mails.out
-#echo ####################################################"
-#echo EMAILS"
-#echo ####################################################"
-
-#for url in $(cat hosts.txt);do
-#host $url | grep has address" >> temp_actives.txt
-#done
-#cat temp_actives.txt | sort -u > hosts_actives.txt
-#cat hosts_actives.txt
-#rm parsing.txt temp_actives.txt
 fi
