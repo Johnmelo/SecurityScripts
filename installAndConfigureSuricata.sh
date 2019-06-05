c388cbf82590d0b6e00844c3499bc7905f6a64f7
Jonathas <johnjonathas0@gmail.com>
2019-06-05 20:10:28 -0300

    Add files via upload

---
+++ installAndConfigureSuricata.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get -y install libpcre3 libpcre3-dbg libpcre3-dev \
+build-essential autoconf automake libtool libpcap-dev libnet1-dev \
+libyaml-0-2 libyaml-dev zlib1g zlib1g-dev libcap-ng-dev libcap-ng0 \
+make libmagic-dev libjansson-dev libjansson4 pkg-config
+
+wget http://www.openinfosecfoundation.org/download/suricata-3.1.tar.gz
+tar -xvzf suricata-3.1.tar.gz
+cd suricata-3.1
+
+./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var
+make
+sudo make install
+sudo ldconfig
